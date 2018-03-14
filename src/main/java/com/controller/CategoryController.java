3eb2080bd5912aaee5d561fbf1f036549b5e24a1
Sai ram Bheemavarapu <sai.bheemavarapu@gmail.com>
2018-03-14 13:32:20 +0530

    ui

--- src/main/java/com/controller/CategoryController.java
+++ src/main/java/com/controller/CategoryController.java
@@ -22,6 +22,7 @@ public class CategoryController {
 	@Autowired
 	private CategoryDAO categoryDAO;
 	
+	
 	@RequestMapping(value="/category",method=RequestMethod.GET)
 	public String insertCategory(@ModelAttribute("category") Category category,Model model,BindingResult result){
 		model.addAttribute("category",new Category());
